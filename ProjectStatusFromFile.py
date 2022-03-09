9f2872307b8aac2c2467fb5a1e0b166c7a8a6711
bandy004 <banerjee.debdeep@yahoo.com>
2022-03-09 14:09:38 +1100

    Updated stacked bar chart for people

--- ProjectStatusFromFile.py
+++ ProjectStatusFromFile.py
@@ -5,13 +5,20 @@ import plotly_express as px
 
 
 def addData(data, series, name):
-    table = {}
+    table = []
     for s in series:
-        s = data.loc[data[name] == s].groupby(name).count()['ID']
+        s = data.loc[data[name[0]] == s].groupby(name).count()['ID']
         for i, v in s.iteritems():
-            #print(type(i), type(v))
-            table[i] = v
-    return pd.DataFrame(list(table.items()), columns=[name, 'Counts']).sort_values(by="Counts", ascending=False)
+            item = {}
+            for index, val in enumerate(name):
+                if(len(name) > 1):
+                    item[val] = i[index]
+                else:
+                    item[val] = i
+            item['Counts'] = v
+            table.append(item)
+
+    return pd.DataFrame(table).sort_values(by="Counts", ascending=False)
 
 
 st.title("Project Status Report")
@@ -34,14 +41,15 @@ if file is not None:
         data = data.dropna(subset=['ID'], how='all')
 
         stats = data['Status'].unique()
-        stats_table = addData(data, stats, 'Status')
+        stats_table = addData(data, stats, ['Status'])
         st.table(stats_table)
         stat_fig = px.bar(stats_table, x='Status', y='Counts')
         st.plotly_chart(stat_fig, use_container_width=True)
 
         people = data['Owner'].unique()
-        people_table = addData(data, people, 'Owner')
+        people_table = addData(data, people, ['Owner', 'Status'])
         st.table(people_table)
-        people_fig = px.bar(people_table, x='Owner', y='Counts')
+        people_fig = px.bar(people_table, x='Owner',
+                            y='Counts', color="Status", barmode='stack')
         st.plotly_chart(people_fig, use_container_width=True)
         # st.dataframe(data)
