891021a14befb79b0d2e24622cb6c4087d8405b9
bandy004 <banerjee.debdeep@yahoo.com>
2022-03-09 11:52:00 +1100

    first commit

---
+++ ProjectStatusFromFile.py
@@ -0,0 +1,47 @@
+from sqlalchemy import null
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+
+def addData(data, series, name):
+    table = {}
+    for s in series:
+        s = data.loc[data[name] == s].groupby(name).count()['ID']
+        for i, v in s.iteritems():
+            #print(type(i), type(v))
+            table[i] = v
+    return pd.DataFrame(list(table.items()), columns=[name, 'Counts']).sort_values(by="Counts", ascending=False)
+
+
+st.title("Project Status Report")
+st.header(
+    "Choose the project status file...")
+st.text("Export from QP with ID, Status, Owner, Deadline")
+file = st.file_uploader("")
+if file is not None:
+    ext = file.name.split(".")[1]
+    # st.text(ext)
+    data = None
+    if(ext == 'csv'):
+        data = pd.read_csv(file)
+    elif ext == 'xlsx' or ext == 'xls':
+        data = pd.read_excel(file)
+    else:
+        st.error("Please choose a 'csv' or 'xlsx' or 'xls' file type")
+    if data is not None:
+        data = data.drop(columns=['Deadline'])
+        data = data.dropna(subset=['ID'], how='all')
+
+        stats = data['Status'].unique()
+        stats_table = addData(data, stats, 'Status')
+        st.table(stats_table)
+        stat_fig = px.bar(stats_table, x='Status', y='Counts')
+        st.plotly_chart(stat_fig, use_container_width=True)
+
+        people = data['Owner'].unique()
+        people_table = addData(data, people, 'Owner')
+        st.table(people_table)
+        people_fig = px.bar(people_table, x='Owner', y='Counts')
+        st.plotly_chart(people_fig, use_container_width=True)
+        # st.dataframe(data)
